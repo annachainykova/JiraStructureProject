6cfe0d7f898962fade516fd64d125d35bcaf4f6e
Anna <a.chainikova@gmail.com>
2018-06-26 15:32:06 +0300

    added listeners

---
+++ src/main/java/hillelauto/Jira/listeners/Retry.java
@@ -0,0 +1,20 @@
+package hillelauto.Jira.listeners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer{
+    private int retryCount = 0;
+    private int maxRetryCount = 1;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if(retryCount >= maxRetryCount)
+                return false;
+
+        System.out.println("retrying test " + result.getName());
+        retryCount++;
+        return true;
+    }
+
+}
